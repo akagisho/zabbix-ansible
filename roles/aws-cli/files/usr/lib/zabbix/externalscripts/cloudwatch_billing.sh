cd25e5f8d1a4e558fe76939d4ede5151400b40bb
akagisho <akagisho@gmail.com>
2016-01-12 19:20:50 +0900

    Update cloudwatch_billing.sh

--- roles/aws-cli/files/usr/lib/zabbix/externalscripts/cloudwatch_billing.sh
+++ roles/aws-cli/files/usr/lib/zabbix/externalscripts/cloudwatch_billing.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+SERVICE_NAME=$1
+
+DIMENSIONS=""
+if [ ! -z "$SERVICE_NAME" ]; then
+  DIMENSIONS=',{"Name":"ServiceName","Value":"'$SERVICE_NAME'"}'
+fi
+
 aws cloudwatch get-metric-statistics \
   --output text \
   --region us-east-1 \
@@ -9,5 +16,5 @@ aws cloudwatch get-metric-statistics \
   --end-time `date -u +%Y-%m-%dT%TZ` \
   --period 300 \
   --statistics Maximum \
-  --dimensions Name=Currency,Value=USD \
+  --dimensions '[{"Name":"Currency","Value":"USD"}'$DIMENSIONS']' \
   | sort -k 3,3 | tail -n 1 | awk '{print $2}'
