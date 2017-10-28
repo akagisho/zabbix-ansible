a55007ec0c6a4c68d63ff4d64c37497f467ef08b
akagisho <akagisho@gmail.com>
2017-10-29 00:48:15 +0900

    Update external script

--- roles/zabbix/files/usr/lib/zabbix/externalscripts/external_dns_check.sh
+++ roles/zabbix/files/usr/lib/zabbix/externalscripts/external_dns_check.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 HOST=$1
+DOMAIN=$2
+TYPE=$3
 
-dig @$HOST $HOST A +time=4 +tries=2 > /dev/null 2>&1
+[ -z "$DOMAIN" ] && DOMAIN=$HOST
+[ -z "$TYPE" ] && TYPE="A"
+
+dig @$HOST $DOMAIN $TYPE +time=4 +tries=2 > /dev/null 2>&1
 echo $?
