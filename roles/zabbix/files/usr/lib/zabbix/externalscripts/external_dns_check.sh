7ddf6ebfd3b24235c45c5a12d527ef22c58a5688
akagisho <akagisho@gmail.com>
2017-12-30 13:49:40 +0900

    Update external_dns_check.sh

--- roles/zabbix/files/usr/lib/zabbix/externalscripts/external_dns_check.sh
+++ roles/zabbix/files/usr/lib/zabbix/externalscripts/external_dns_check.sh
@@ -7,5 +7,18 @@ TYPE=$3
 [ -z "$DOMAIN" ] && DOMAIN=$HOST
 [ -z "$TYPE" ] && TYPE="A"
 
-dig @$HOST $DOMAIN $TYPE +time=4 +tries=2 > /dev/null 2>&1
-echo $?
+RESULT=`dig @$HOST $DOMAIN $TYPE +time=4 +tries=2 2> /dev/null`
+
+rval=$?
+if [ $rval != 0 ]; then
+  echo 0
+  exit
+fi
+
+LF=$(printf '\\\012_')
+LF=${LF%_}
+
+echo "$RESULT" \
+  | sed 's/, /'"$LF"'/g' \
+  | grep ANSWER: \
+  | cut -d' ' -f2
